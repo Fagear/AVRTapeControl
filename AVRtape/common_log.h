bdd2f854a4b05cb8d40ed3ec326e7ab92a3ec3b4
Fagear <fagear@mail.ru>
2024-02-21 01:12:24 +0300

    Readme fix
    Updated pinout and fixed video hint error

--- AVRtape/common_log.h
+++ AVRtape/common_log.h
@@ -104,7 +104,7 @@ enum
 	TTR_FEA_TWO_PLAYS = (1<<5),		// Enable two PLAY buttons/LEDs (for each direction)
 };
 //#define TTR_REV_DEFAULT		(TTR_FEA_STOP_TACHO|TTR_FEA_REV_ENABLE|TTR_FEA_PB_AUTOREV|TTR_FEA_END_REW)		// Default feature settings
-#define TTR_REV_DEFAULT		(TTR_FEA_REV_ENABLE|TTR_FEA_END_REW)		// Default feature settings
+#define TTR_REV_DEFAULT				(TTR_FEA_REV_ENABLE|TTR_FEA_PB_AUTOREV|TTR_FEA_PB_LOOP)		// Default feature settings
 
 // Index of byte in SPI bus extenders for [u8a_spi_buf].
 enum
